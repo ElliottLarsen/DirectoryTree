7a83101174c75287380ba0cfdc674c6b2b422a14
Elliott Larsen <elliottlrsn@gmail.com>
2023-10-29 21:02:56 -0600

    Initial commit.

---
+++ tree.rs
@@ -0,0 +1,7 @@
+use std::{env, path};
+fn main() {
+    // Read artument values.
+    let args: Vec<String> = env::args().collect();
+    // If no path is given, set it to the current directory.
+    let mut target_dir = ".";
+}
